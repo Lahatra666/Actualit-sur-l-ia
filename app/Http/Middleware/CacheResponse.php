92a7aff9c475de0b4b7648a49ef5b86ee2bbc838
Lahatra666 <rabenarivolahatra@gmail.com>
2023-05-08 18:25:48 +0300

    Create CacheResponse.php

---
+++ app/Http/Middleware/CacheResponse.php
@@ -0,0 +1,11 @@
+namespace Illuminate\Http\Middleware;
+
+class CacheResponse extends EnsureHttpHeadersAreValid
+{
+    /**
+     * The maximum amount of time the response should be cached.
+     *
+     * @var int
+     */
+    const CACHE_CONTROL_MAX_AGE = 3600;
+}
