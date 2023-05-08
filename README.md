a0e66fbe3e8f4beab3345c14828a8c198d451a04
Lahatra666 <rabenarivolahatra@gmail.com>
2023-05-08 18:37:32 +0300

    cd

--- README.md
+++
@@ -1,2 +0,0 @@
-# TP-S6-P14-Web-Design-Mai-Lahatra-1568
- 
--- webpack.mix.js
+++
@@ -1,6 +0,0 @@
-const mix = require('laravel-mix');
-
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('ressources/css/app.css', 'public/css', [
-        //
-    ]);
