9ef2d1804c7a7ed5fa54c67a3fb7d4e78721de30
Lahatra666 <99417874+Lahatra666@users.noreply.github.com>
2023-05-08 18:31:21 +0300

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,6 @@ class Kernel extends HttpKernel
         \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
-        \Illuminate\Http\Middleware\CacheResponse::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
