1d4fe931939833f0b4de33c1ae2f9970c6515494
Lahatra666 <99417874+Lahatra666@users.noreply.github.com>
2023-05-08 15:29:59 +0300

    Update client.blade.php

--- resources/views/layout/client.blade.php
+++ resources/views/layout/client.blade.php
@@ -42,7 +42,7 @@
     </div><!-- End Logo -->
 
     <div class="search-bar">
-      <form class="search-form d-flex align-items-center" method="GET" action="#">
+      <form class="search-form d-flex align-items-center" method="POST" action="#">
         <input type="text" name="mot" placeholder="Search" title="Enter search keyword">
         <button type="submit" title="Search"><i class="bi bi-search"></i></button>
       </form>
